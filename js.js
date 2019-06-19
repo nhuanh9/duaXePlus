ba03a0a710389a2dcdb4ba14631f805fc1f6c9d6
nhuanh9 <49300627+nhuanh9@users.noreply.github.com>
2019-06-19 21:17:43 +0700

    fix code

--- js.js
+++ js.js
@@ -1,45 +1,75 @@
-const DEFAULT_X = document.getElementById("myCanvas").offsetWidth;
-const DEFAULT_Y = document.getElementById("myCanvas").offsetHeight;
+const WIDTH_MAP = document.getElementById("myCanvas").offsetWidth;
+const HEIGHT_MAP = document.getElementById("myCanvas").offsetHeight;
 
-let theCar = function (DEFAULT_X, DEFAULT_Y, speed) {
-    this.c = document.getElementById("myCanvas");
-    this.ctx = this.c.getContext("2d");
+const CAR_SPEED = 20;
+const SIZE_IMG = 48;
+
+const CIRCLE_SPEED = 1;
+const CIRCLE_SIZE = 20;
+let theCar = function (WIDTH_MAP, HEIGHT_MAP, speed) {
+    this.ctx = document.getElementById("myCanvas").getContext("2d");
     this.img = new Image();
-    this.posX = DEFAULT_X * 0.45;
-    this.posY = DEFAULT_Y * 0.85;
+    this.posX = WIDTH_MAP * 0.45;
+    this.posY = HEIGHT_MAP * 0.85;
     this.img.src = "Car-Top-Red-icon.png";
     this.drawCar = function (img, nowXofCar, nowYofCar) {
         this.ctx.drawImage(img, nowXofCar, nowYofCar);
     };
     this.diLen = function () {
-        if (this.posY >= DEFAULT_Y*0.5) {
-            this.ctx.clearRect(0, 0, 1350, 600);
+        if (this.posY >= HEIGHT_MAP*0.5) {
+            this.ctx.clearRect(this.posX, this.posY, 48, 48);
             this.posY -= speed;
             this.drawCar(this.img, this.posX, this.posY);
         }
     };
     this.diXuong = function () {
-        if (this.posY <= DEFAULT_Y*0.85) {
-            this.ctx.clearRect(0, 0, 1350, 600);
+        if (this.posY <= HEIGHT_MAP*0.85) {
+            this.ctx.clearRect(this.posX, this.posY, 48, 48);
             this.posY += speed;
             this.drawCar(this.img, this.posX, this.posY);
         }
     };
     this.diPhai = function () {
-        if (this.posX <= DEFAULT_X*0.85) {
-            this.ctx.clearRect(0, 0, 1350, 600);
+        if (this.posX <= WIDTH_MAP*0.85) {
+            this.ctx.clearRect(this.posX, this.posY, 48, 48);
             this.posX += speed;
             this.drawCar(this.img, this.posX, this.posY);
         }
     };
     this.diTrai = function () {
         if (this.posX >= 1) {
-            this.ctx.clearRect(0, 0, 1350, 600);
+            this.ctx.clearRect(this.posX, this.posY, 48, 48);
             this.posX -= speed;
             this.drawCar(this.img, this.posX, this.posY);
         }
     };
-    this.checkViTri = function () {
-
+};
+let circle = function (WIDTH_MAP, HEIGHT_MAP) {
+    this.posX = Math.random()*WIDTH_MAP - CIRCLE_SIZE;
+    this.posY = HEIGHT_MAP * 0.01;
+    this.speed = CIRCLE_SPEED;
+    this.ctx = document.getElementById("myCanvas").getContext("2d");
+    this.draw = function () {
+        this.ctx.fillStyle = "#FF0000";
+        this.ctx.fillRect(this.posX,this.posY,CIRCLE_SIZE, CIRCLE_SIZE);
+    };
+    this.attack = function (carX,carY) {
+        this.turnDown();
+        if (this.posY >HEIGHT_MAP*0.95 ){
+            this.posX = Math.random()*WIDTH_MAP - CIRCLE_SIZE;
+            this.posY = HEIGHT_MAP * 0.01;
+        } else if (this.isLose(carX,carY)){
+            confirm("Lose");
+        }
+        this.draw(this.posX,this.posY);
+    };
+    this.turnDown = function () {
+        this.ctx.clearRect(this.posX-2, this.posY, CIRCLE_SIZE*1.2, CIRCLE_SIZE);
+        this.posY += this.speed;
+    };
+    this.isLose = function (carX, carY) {
+        let isLoseWidth = (this.posX >= carX-SIZE_IMG/8)  && (this.posX <= carX+SIZE_IMG*7/8);
+        let isLoseHeight = (this.posY+CIRCLE_SIZE >= carY)  && (this.posY+CIRCLE_SIZE <= carY+SIZE_IMG);
+        return isLoseHeight&&isLoseWidth;
     }
 };
